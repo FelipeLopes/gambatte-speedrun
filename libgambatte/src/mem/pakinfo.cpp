91f1774b34ea77af32edbafc229339101fa0fb79
Pigu-A <supa7764@gmail.com>
2017-04-18 20:16:04 +0700

    Replace that C++11 function so ax6 can build

--- libgambatte/src/mem/pakinfo.cpp
+++ libgambatte/src/mem/pakinfo.cpp
@@ -87,7 +87,7 @@ std::string const TPP1ToCstr(unsigned char const h152, unsigned char const h153)
 	if ((h153 & 3) == 3) t += "rumble+,";
 	if (h153 & 1) t += "rumble,";
 	if (h152 | (h153 & 4)) t += "battery,";
-	t.back() = ']';
+	t[t.size() - 1] = ']';
 	return t;
 }
 
