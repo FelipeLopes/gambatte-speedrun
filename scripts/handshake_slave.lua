6d1d5ec63074b69af9f15c203e0dd48ca3620d0d
FelipeLopes <felipelopesfreitas@gmail.com>
2025-08-29 16:34:25 +0200

    slave script

---
+++ scripts/handshake_slave.lua
@@ -0,0 +1,17 @@
+local doing_handshake = true
+
+function serial(a)
+  if doing_handshake then
+    doing_handshake = false
+    return 0x29
+  else
+    return 0xac
+  end
+end
+
+function serial_on_timer()
+  gb.request_serial_interrupt()
+end
+
+gb.on_serial_exchange(serial)
+gb.on_timer_interrupt(serial_on_timer)
