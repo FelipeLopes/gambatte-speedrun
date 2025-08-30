a3a7a0c843fe897a338551adfd8fa675ff619e5a
FelipeLopes <felipelopesfreitas@gmail.com>
2025-08-30 17:27:17 +0200

    duplex + node

--- scripts/rng_on_timer.lua
+++ scripts/rng_on_timer.lua
@@ -2,11 +2,18 @@ local M = require "posix.unistd"
 local F = require "posix.fcntl"
 
 local from_ws = F.open('/home/felipe/ws_to_lua', bit32.bor(F.O_RDONLY, F.O_NONBLOCK))
+local to_ws = F.open('/home/felipe/lua_to_ws', F.O_WRONLY)
+
+local a = 0
 
 function print_rng()
+  a = math.fmod(a+1,240)
   local data, err = M.read(from_ws, 1)
   if data ~= nil then
     print(data)
+  end
+  if a == 0 then
+    M.write(to_ws, string.char(gb.read_memory(0xcaca)))
   end
 end
 
